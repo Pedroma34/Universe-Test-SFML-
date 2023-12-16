c41b0a84ed31c42191dd7d7ed499ed3c9a7d19f5
Pedroma34 <62633760+Pedroma34@users.noreply.github.com>
2023-12-16 02:46:41 -0500

    Organized probability algorithim for size, color, and position

--- Universe Test/source/StarSystem.cpp
+++ Universe Test/source/StarSystem.cpp
@@ -29,96 +29,96 @@ void StarSystem::SetStarPositionInSector(int64_t l_row, int64_t l_column, int64_
     double OffsetX = fmod(UserX, sectorSize);
     double OffsetY = fmod(UserY, sectorSize);
 
+    //Chance and position
+    std::vector<std::pair<double, sf::Vector2f>> positions{
+        /*Top Left*/     {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + starGlobalBounds.height - OffsetY)                           }, 
+        /*Top Right*/    {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + sectorSize - starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + starGlobalBounds.height - OffsetY)              }, 
+        /*Bottom Left*/  {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize - starGlobalBounds.height - OffsetY)              }, 
+        /*Bottom Right*/ {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + sectorSize - starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize - starGlobalBounds.height - OffsetY) }, 
+        /*Center Top*/   {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + sectorSize / 2 - OffsetX, (l_column - l_startColumn) * sectorSize + starGlobalBounds.height - OffsetY)                                   }, 
+        /*Center Left*/  {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + starGlobalBounds.width + starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize / 2 - OffsetY)           }, 
+        /*Center Right*/ {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + sectorSize - starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize / 2 - OffsetY)                       }, 
+        /*Center*/       {0.125, sf::Vector2f((l_row - l_startRow) * sectorSize + sectorSize / 2 - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize / 2 - OffsetY)                                            }, 
+	};
+
     std::uniform_real_distribution<double> dist(0.0, 1.0);
-
     double positionRoll = dist(rng);
-    const double probabilityTopLeft     = 0.125;
-    const double probabilityTopRight    = 0.125;
-    const double probabilityBottomLeft  = 0.125;
-    const double probabilityBottomRight = 0.125;
-    const double probabilityCenterTop   = 0.125;
-    const double probabilityCenterLeft  = 0.125;
-    const double probabilityCenterRight = 0.125;
-    const double probabilityCenter      = 0.125;
 
-    //Top left
-    if (positionRoll < probabilityTopLeft) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + starGlobalBounds.height - OffsetY);
-    
+    double cumulativeProbability = 0.0;
 
-    //Top Right
-    else if (positionRoll < (probabilityTopLeft + probabilityTopRight)) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + sectorSize - starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + starGlobalBounds.height - OffsetY);
-    
+    for (auto& position : positions) {
 
-    //Bottom Left
-    else if (positionRoll < (probabilityTopLeft + probabilityTopRight + probabilityBottomLeft)) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize - starGlobalBounds.height - OffsetY);
-    
+        cumulativeProbability += position.first;
 
-    //Bottom Right
-    else if (positionRoll < (probabilityTopLeft + probabilityTopRight + probabilityBottomLeft + probabilityBottomRight)) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + sectorSize - starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize - starGlobalBounds.height - OffsetY);
-    
+        if (positionRoll > cumulativeProbability)
+			continue;
 
-    //Center Top
-    else if (positionRoll < (probabilityTopLeft + probabilityTopRight + probabilityBottomLeft + probabilityBottomRight + probabilityCenterTop)) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + sectorSize / 2 - OffsetX, (l_column - l_startColumn) * sectorSize + starGlobalBounds.height - OffsetY);
-    
-
-    //Center Left
-    else if (positionRoll < (probabilityTopLeft + probabilityTopRight + probabilityBottomLeft + probabilityBottomRight + probabilityCenterTop + probabilityCenterLeft)) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + starGlobalBounds.width + starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize / 2 - OffsetY);
-    
-
-    //Center Right
-    else if (positionRoll < (probabilityTopLeft + probabilityTopRight + probabilityBottomLeft + probabilityBottomRight + probabilityCenterTop + probabilityCenterLeft + probabilityCenterRight)) 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + sectorSize - starGlobalBounds.width - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize / 2 - OffsetY);
-    
-
-    //Center
-    else 
-        StarShape.setPosition((l_row - l_startRow) * sectorSize + sectorSize / 2 - OffsetX, (l_column - l_startColumn) * sectorSize + sectorSize / 2 - OffsetY);
-    
+        StarShape.setPosition(position.second);
+        return;
+    }
 
 }
 
 void StarSystem::DetermineStarSize(std::mt19937_64& l_rng) {
+
+    //Chance and Size
+    std::vector<std::pair<double, float>> sizeProbabilities = {
+        {0.55, 06.0f /*Medium*/       }, //55.0%
+        {0.25, 09.0f /*Large*/        }, //25.0%  
+        {0.18, 04.0f /*Small*/        }, //18.0% 
+        {0.02, 17.0f /*Hyper Large*/  }  //02.0%  
+    };
+
     std::uniform_real_distribution<double> dist(0.0, 1.0);
     double sizeRoll = dist(l_rng);
-    const double probabilitySmall = 0.40;
-    const double probabilityMedium = 0.55;
-    const double probabilityLarge = 0.05;
-    if (sizeRoll < probabilitySmall)
-        Size = 4;
-    else if (sizeRoll < (probabilitySmall + probabilityMedium))
-        Size = 6;
-    else
-        Size = 9;
-    StarShape.setRadius(Size);
-    //Setting origin to middle
-    StarShape.setOrigin(StarShape.getGlobalBounds().width / 2, StarShape.getGlobalBounds().height / 2);
+
+    double cumulativeProbability = 0.0;
+
+    for (auto& sizeProbability : sizeProbabilities) {
+
+		cumulativeProbability += sizeProbability.first;
+
+		if (sizeRoll > cumulativeProbability)
+			continue;
+
+		Size = sizeProbability.second;
+		StarShape.setRadius(Size);
+		//Setting origin to middle
+		StarShape.setOrigin(StarShape.getGlobalBounds().width / 2, StarShape.getGlobalBounds().height / 2);
+
+		return;
+	}
 }
 
 void StarSystem::DetermineStarColor(std::mt19937_64& l_rng) {
     
+    double maxChance = 1.0; //100%
+    double chance = 0.0;
+
+    //Chance and Color
+    std::vector<std::pair<double, sf::Color>> colorProbabilities = {
+		{ 0.525, sf::Color::Red                     }, //52.5%
+		{ 0.325, sf::Color(255, 69, 0)    /*Red*/   }, //32.5%
+        { 0.075, sf::Color(255, 255, 159) /*White*/ }, //07.5%
+		{ 0.050, sf::Color::Blue                    }, //05.0%
+        { 0.025, sf::Color(50, 23, 77)   /*Purple*/ }, //02.5%
+	};
+
     std::uniform_real_distribution<double> dist(0.0, 1.0);
     double colorRoll = dist(l_rng);
 
-    const double probabilityRed = 0.65;
-    const double probabilityYellow = 0.10;
-    const double probabilityOrange = 0.10;
-    const double probabilityWhite = 0.10;
-    const double probabilityBlue = 0.05;
+    double cumulativeProbability = 0.0;
 
-    if (colorRoll < probabilityYellow)
-        StarShape.setFillColor(sf::Color::Yellow);
-    else if (colorRoll < (probabilityYellow + probabilityBlue))
-        StarShape.setFillColor(sf::Color::Blue);
-    else if (colorRoll < (probabilityYellow + probabilityBlue + probabilityOrange))
-        StarShape.setFillColor(sf::Color(255, 165, 0));
-    else if (colorRoll < (probabilityYellow + probabilityBlue + probabilityOrange + probabilityWhite))
-        StarShape.setFillColor(sf::Color::White);
-    else
-        StarShape.setFillColor(sf::Color::Red);
+    for (auto& colorProbability : colorProbabilities) {
+
+		cumulativeProbability += colorProbability.first;
+
+        if (colorRoll > cumulativeProbability)
+            continue;
+
+		StarShape.setFillColor(colorProbability.second);
+
+		return;
+	}
+
 }
